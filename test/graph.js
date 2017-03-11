def1a98ee1dde5863f3806e45cfed4181fa5e5aa
bavla <vladimir.batagelj@gmail.com>
2017-03-11 11:11:31 +0100

    Add files via upload

---
+++ test/graph.js
@@ -0,0 +1,14 @@
+graph = {
+  "nodes":[
+    {"name":"Ann","Num":1,"Size":100},
+    {"name":"Ben","Num":4,"Size":500},
+    {"name":"Tim","Num":2,"Size":200},
+    {"name":"Zoe","Num":3,"Size":400}
+  ],
+  "links":[
+    {"source":0,"target":1,"Count":1,"Weight":100},
+    {"source":1,"target":2,"Count":1,"Weight":100},
+    {"source":2,"target":3,"Count":1,"Weight":100},
+    {"source":1,"target":3,"Count":2,"Weight":300}
+  ]
+}
